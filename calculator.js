634ee88e172094faa7560285db8f3b3b959f2b52
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-06-28 16:19:13 +0100

    Refactor variable initialization and enhance button event handling in calculator.js

--- calculator.js
+++ calculator.js
@@ -3,10 +3,6 @@ let subtract = (a, b) => a - b;
 let divide = (a, b) => a / b;
 let multiply = (a, b) => a * b;
 
-const num1 = 0;
-const num2 = 0;
-let operator = "";
-
 let operate = (a, operator, b) => {
   if (operator === "+") {
     return add(a, b);
@@ -18,3 +14,52 @@ let operate = (a, operator, b) => {
     return multiply(a, b);
   }
 };
+
+let num1 = null;
+let num2 = null;
+let operator = "";
+
+const buttons = document.querySelectorAll("button");
+const display = document.querySelector("p");
+let storedDisplay = "";
+let result = 0;
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", (event) => {
+    let targetButton = event.target.textContent;
+    storedDisplay += targetButton;
+    display.textContent += targetButton;
+
+    if (["+", "-", "*", "/"].includes(targetButton)) {
+      if (num1 === null && num2 === null) {
+        num1 = parseInt(storedDisplay);
+      } else if (storedDisplay !== "") {
+        num2 = parseInt(storedDisplay);
+        num1 = operate(num1, operator, num2);
+        display.textContent = num1;
+      }
+
+      operator = targetButton;
+      storedDisplay = "";
+    }
+
+    if (targetButton === "=") {
+      num2 = parseInt(storedDisplay);
+      result = operate(num1, operator, num2);
+      display.textContent = result;
+      storedDisplay = "";
+      num1 = parseInt(result);
+    }
+
+    if (targetButton === "c") {
+      display.textContent = "";
+      storedDisplay = "";
+      num1 = null;
+      num2 = null;
+    }
+
+    console.log(`num1 = ${num1}`);
+    console.log(`num2 = ${num2}`);
+    console.log(`stored = ${storedDisplay}`);
+  });
+});
