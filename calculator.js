48f5bd37c09eeaed05243b13e460d20ad2596b51
the-blockman <105639638+the-blockman@users.noreply.github.com>
2025-06-28 03:26:57 +0100

    Implement operate function and initialize constants in calculator.js

--- calculator.js
+++ calculator.js
@@ -2,3 +2,19 @@ let add = (a, b) => a + b;
 let subtract = (a, b) => a - b;
 let divide = (a, b) => a / b;
 let multiply = (a, b) => a * b;
+
+const num1 = 0;
+const num2 = 0;
+let operator = "";
+
+let operate = (a, operator, b) => {
+  if (operator === "+") {
+    return add(a, b);
+  } else if (operator === "-") {
+    return subtract(a, b);
+  } else if (operator === "/") {
+    return divide(a, b);
+  } else if (operator === "*") {
+    return multiply(a, b);
+  }
+};
